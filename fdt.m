3ac563cedecfc7b4e5e094fa7780d3fa6291e013
ssc64@tig.mizzou.edu <ssc64@tig.mizzou.edu@EBW120-LN-27.doit.missouri.edu>
2016-10-26 22:26:00 -0500

    Decision tree 1, zadeh operators  and default membership functions working.

--- fdt.m
+++ fdt.m
@@ -1,7 +1,7 @@
 function fdt(inFile, mf, op)
 % Fuzzy decision trees
 
-inp = csvread(inFile);
+in = csvread(inFile);
 % Input data has 7 variables
 % Car ID, Risk, Value Loss, Horsepower, City MPG, Highway MPG, and Price
 
@@ -36,18 +36,37 @@ price.Cheap = [0 0 7000 10000];
 price.Average = [7000 10000 20000];
 price.Expensive = [10000 20000 40000 40000];
 
+% Decision Tree for evaluting the car for the membership function,
+% operation and assumed decision tree
+w = 1;
+assesment = decisionTree(in, 1, 'zadeh', w, risk, valueLoss, horsepower, cityMPG, highwayMPG, price);
+
+[bestVal, bestIdx] = max(assesment, [], 1); % Finding the best Car
+disp(['Best Car ID is ', num2str(in(bestIdx, 1)), ' and its value is ', num2str(bestVal)]);
+[counts, centers]=hist(assesment, 10);  % Histogram of output car ratings
+hist(centers, assesment, 10)
+title('Histogram for car assesment')
 end
 
 % Decision Tree for the given input
-function assesment = decisionTree(inp, decisionTree, opModel, risk, valueLoss, horsepower, cityMPG, highwayMPG, price)
+function assesment = decisionTree(in, decisionTree, opModel, w, risk, valueLoss, horsepower, cityMPG, highwayMPG, price)
 
 % Number of inputs
-N = size(inp, 1);
+N = size(in, 1);
+assesment = zeros(N,1);
 
 for i=1:N % Loop for all the input data for the decision tree
-    if decisionTree==1
-        opModel = 'zadeh';
-        assesment(i) = fuzzyOp([fuzzyOp([fuzzyOp([mf({highwayMPG.Good, in(i,6)}, 'trapmf'), mf({horsepower.Average, in(i,4)}, 'trimf')], 'AND_F', opModel), fuzzyOp(mf({cityMPG.Poor, in(i,5)}, 'trapmf'), 'NOT_F', opModel)], 'AND_F', opModel), fuzzyOp([fuzzyOp([mf({risk.Low, in(i,2)}, 'trapmf'), mf({valueLoss.Low, in(i,3)}, 'trapmf')],'AND_F', opModel), mf({price.Cheap, in(i,7)}, 'trapmf')], 'OR_F', opModel)], 'AND_F', opModel);
+    if decisionTree==1  % Default decision tree
+        assesment(i) = fuzzyOp([
+                        fuzzyOp([...
+                            fuzzyOp([eval_mf({highwayMPG.Good, in(i,6)}, 'trapmf'), eval_mf({horsepower.Average, in(i,4)}, 'trimf')], 'AND_F', opModel, w),... 
+                            fuzzyOp(eval_mf({cityMPG.Poor, in(i,5)}, 'trapmf'), 'NOT_F', opModel, w)],... 
+                        'AND_F', opModel, w),... 
+                        fuzzyOp([...
+                            fuzzyOp([eval_mf({risk.Low, in(i,2)}, 'trapmf'), eval_mf({valueLoss.Low, in(i,3)}, 'trapmf')],'AND_F', opModel, w),... 
+                            eval_mf({price.Cheap, in(i,7)}, 'trapmf')],... 
+                        'OR_F', opModel, w)],... 
+                       'AND_F', opModel, w);
     elseif decisionTree==2
     end
 end
@@ -55,33 +74,33 @@ end
 end
 
 % Fuzzy operators
-function out = fuzzyOp(in, op, model)
+function out = fuzzyOp(in, op, model, w)
 
 % Models
 % zadeh, Bounded, and Yager
 
-if op=='AND_F'    % Fuzzy intersection or conjuction
-    if model=='zadeh'
-        out = min(in, 2);
-    elseif model=='bounded'
+if strcmp(op, 'AND_F')    % Fuzzy intersection or conjuction
+    if strcmp(model, 'zadeh')
+        out = min(in, [], 2);
+    elseif strcmp(model, 'bounded')
         out = max(0, sum(in, 2)-1);
-    elseif model=='yager'
+    elseif strcmp(model, 'yager')
         out = min(1, sum(in.^w, 2).^1/w);
     end
-elseif op=='OR_F'     % Fuzzy union
-    if model=='zadeh' %#ok<*STCMP>
-        out = max(in, 2);
-    elseif model=='bounded'
+elseif strcmp(op, 'OR_F')     % Fuzzy union
+    if strcmp(model, 'zadeh') %#ok<*STCMP>
+        out = max(in,[], 2);
+    elseif strcmp(model, 'bounded')
         out = min(0, sum(in, 2));
-    elseif model=='yager'
+    elseif strcmp(model, 'yager')
         out = 1 - min(1, sum((1-in).^w, 2).^1/w);
     end
-elseif op=='NOT_F'    % Fuzzy complement
-    if model=='zadeh'
+elseif strcmp(op, 'NOT_F')    % Fuzzy complement
+    if strcmp(model, 'zadeh')
         out = 1-in;
-    elseif model=='bounded'
+    elseif strcmp(model, 'bounded')
         out = 1-in;
-    elseif model=='yager'
+    elseif strcmp(model, 'yager')
         out = (1-in.^w).^1/w;
     end
 end
@@ -104,29 +123,30 @@ function mv = eval_mf(mf, fn)
 % Asymmetric functions: sigmf, dsigmf, psigmf
 % Polynomial: zmf, pimf, smf
 
-if fn==trimf
+if strcmp(fn, 'trimf')
     % Triangular
-    if val > mf{1}(3)
+    if mf{2} > mf{1}(3)
         mv = 0;
-    elseif val > mf{1}(2)
+    elseif mf{2} > mf{1}(2)
         mv = (mf{1}(3) - mf{2}) / (mf{1}(3) - mf{1}(2));  
-    elseif val > mf{1}(1)
+    elseif mf{2} > mf{1}(1)
         mv = (mf{2} - mf{1}(1)) / (mf{1}(2) - mf{1}(1));
     else 
         mv = 0;
     end
-elseif fn==trapmf
+elseif strcmp(fn, 'trapmf')
     % Trapezoid
-    if val > mf{1}(4)
+    if mf{2} > mf{1}(4)
         mv = 0;
-    elseif val > mf{1}(3)
+    elseif mf{2} > mf{1}(3)
         mv = (mf{1}(4) - mf{2}) / (mf{1}(4) - mf{1}(3));
-    elseif val > mf{1}(2)
+    elseif mf{2} > mf{1}(2)
         mv = 1;
-    elseif val > mf{1}(1)
+    elseif mf{2} > mf{1}(1)
         mv = (mf{2} - mf{1}(1)) / (mf{1}(2) - mf{1}(1));
     else 
         mv = 0;
     end   
+end
 end
 
